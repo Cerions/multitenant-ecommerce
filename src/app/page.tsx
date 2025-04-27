3d51e15de6a72a4b2bf84f89df2e1747a5881e8e
Luca Cerioni <cerions2@gmail.com>
2025-04-27 23:04:41 +0200

    Setup project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return(
+    <div>
+        <p className="text-amber-400">
+          Hello World
+        </p>
+    </div>
+  );
+}
